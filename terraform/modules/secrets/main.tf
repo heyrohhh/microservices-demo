706c6eadfefff1b7285030c2ea30e1329b2e3c44
heyrohhh <rm8619700@gmail.com>
2026-02-13 10:32:09 +0530

    Added secretmanager

--- terraform/modules/secrets/main.tf
+++ terraform/modules/secrets/main.tf
@@ -1,5 +1,6 @@
 resource "aws_secretsmanager_secret" "dockerhub" {
-  name = "dockerhub-creds"
+  name = "dockerhub-creds-v1"
+  recovery_window_in_days = 0 
 }
 
 resource "aws_secretsmanager_secret_version" "dockerhub" {
